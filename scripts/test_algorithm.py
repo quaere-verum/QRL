a399c6508d4ddd6d0cc6440548bbb12f73a71acf
quaere-verum <=>
2025-06-29 09:39:51 +0200

    Made benchmark more interesting/difficult

--- scripts/test_algorithm.py
+++ scripts/test_algorithm.py
@@ -13,22 +13,44 @@ def make_mdp(nr_paths: int, nr_steps: int) -> LQGMDP:
     noise_paths = torch.from_numpy(
         norm.ppf(LatinHypercube(nr_steps, scramble=False, rng=rng).random(nr_paths))
     ).float() * 0.15
+
+    state_transform = {
+        t: torch.tensor([[1.0, 1.0], [0.0, 1.0 - 0.005 * t]]).float()
+        for t in range(nr_steps)
+    }
+
+    control_transform = {
+        t: torch.tensor([[0.0], [1.0 - 0.01 * t]]).clamp(min=0.5).float()
+        for t in range(nr_steps)
+    }
+
+    state_penalty = {
+        t: torch.tensor([[1.0 + 0.05 * t, 0.0], [0.0, 0.1]]).float()
+        for t in range(nr_steps)
+    }
+
+    control_penalty = {
+        t: torch.tensor([[0.01 + 0.001 * t]]).float()
+        for t in range(nr_steps)
+    }
+
     return LQGMDP(
         action_lb=torch.tensor([-100.0]).float(),
         action_ub=torch.tensor([100.0]).float(),
         noise_paths=noise_paths,
-        state_transform=torch.tensor([[1.0, 1.0], [0.0, 1.0]]).float(),
-        control_transform=torch.tensor([[0.0], [1.0]]).float(),
-        state_penalty=torch.tensor([[1.0, 0.0], [0.0, 0.1]]).float(),
-        final_state_penalty=torch.tensor([[1.0, 0.0], [0.0, 1.0]]).float(),
-        control_penalty=torch.tensor([[0.01]]).float(),
-        initial_state=torch.tensor([0.5, 0.5]).float(),
+        state_transform=state_transform,
+        control_transform=control_transform,
+        state_penalty=state_penalty,
+        final_state_penalty=torch.tensor([[10.0, 0.0], [0.0, 2.0]]).float(),
+        control_penalty=control_penalty,
+        initial_state=torch.tensor([2.0, -1.0]).float(),
     )
+
 
 def main():
     algorithm_name = "TD3"
     nr_paths = 1000
-    nr_steps = 10
+    nr_steps = 50
     training_rounds = 250
     mdp = make_mdp(nr_paths, nr_steps)
     benchmark_actions, benchmark_rewards = mdp.solve()
--- src/qrl/mdp.py
+++ src/qrl/mdp.py
@@ -54,21 +54,30 @@ class MDP:
 @dataclass
 class LQGMDP(MDP):
     noise_paths: torch.Tensor
-    state_transform: torch.Tensor
-    control_transform: torch.Tensor
-    state_penalty: torch.Tensor
+    state_transform: dict[int, torch.Tensor]
+    control_transform: dict[int, torch.Tensor]
+    state_penalty: dict[int, torch.Tensor]
     final_state_penalty: torch.Tensor
-    control_penalty: torch.Tensor
+    control_penalty: dict[int, torch.Tensor]
     initial_state: torch.Tensor
 
     def __post_init__(self):
         super().__post_init__()
-        assert self.state_transform.ndim == self.control_transform.ndim == 2
-        assert self.state_transform.shape[0] == self.state_transform.shape[1]
-        assert self.control_transform.shape[0] == self.state_transform.shape[0]
-        assert self.state_transform.shape == self.state_penalty.shape == self.final_state_penalty.shape
-        assert self.control_penalty.shape[0] == self.control_penalty.shape[1] == self.control_transform.shape[1]
-        assert self.initial_state.ndim == 1 and self.initial_state.shape[0] == self.state_transform.shape[0]
+        for obj in [self.state_transform, self.control_transform, self.state_penalty, self.control_penalty]:
+            assert isinstance(obj, dict)
+            assert len(obj) == self.nr_steps
+            for key in obj.keys(): 
+                assert isinstance(key, int)
+        for obj in [self.state_transform, self.control_transform, self.state_penalty, self.control_penalty]:
+            for time in obj:
+                assert obj[time].shape == obj[0].shape
+        
+        assert self.state_transform[0].ndim == self.control_transform[0].ndim == 2
+        assert self.state_transform[0].shape[0] == self.state_transform[0].shape[1]
+        assert self.control_transform[0].shape[0] == self.state_transform[0].shape[0]
+        assert self.state_transform[0].shape == self.state_penalty[0].shape == self.final_state_penalty.shape
+        assert self.control_penalty[0].shape[0] == self.control_penalty[0].shape[1] == self.control_transform[0].shape[1]
+        assert self.initial_state.ndim == 1 and self.initial_state.shape[0] == self.state_transform[0].shape[0]
 
         self._state = torch.zeros((self.nr_paths, self.initial_state.shape[0]), dtype=torch.float32)        
 
@@ -82,11 +91,11 @@ class LQGMDP(MDP):
 
     @property
     def state_dim(self) -> int:
-        return self.state_transform.shape[0] + 1
+        return self.state_transform[0].shape[0] + 1
     
     @property
     def action_dim(self) -> int:
-        return self.control_transform.shape[1]
+        return self.control_transform[0].shape[1]
     
     def _get_obs(self) -> torch.Tensor:
         time_remaining = torch.full((self.nr_paths, 1), self._t / self.nr_steps, dtype=torch.float32)
@@ -101,16 +110,16 @@ class LQGMDP(MDP):
         
         cost = (
             torch.einsum(
-                "bi, ij, bj -> b", self._state, self.state_penalty, self._state
+                "bi, ij, bj -> b", self._state, self.state_penalty[self._t - 1], self._state
             ) + 
             torch.einsum(
-                "bi, ij, bj -> b", action, self.control_penalty, action
+                "bi, ij, bj -> b", action, self.control_penalty[self._t - 1], action
             )
         )
 
         self._state = (
-            torch.matmul(self._state, self.state_transform.T) +
-            torch.matmul(action, self.control_transform.T) +
+            torch.matmul(self._state, self.state_transform[self._t - 1].T) +
+            torch.matmul(action, self.control_transform[self._t - 1].T) +
             self.noise_paths[:, self._t - 1, torch.newaxis]
         )
         if self._t == self.nr_steps:
@@ -128,15 +137,15 @@ class LQGMDP(MDP):
     
     def solve_riccati_equation(self) -> dict[int, torch.Tensor]:
         solutions = [self.final_state_penalty.clone()]
-        for _ in range(self.nr_steps):
+        for t in reversed(range(self.nr_steps)):
             solution = (
-                self.state_transform.T @ (
+                self.state_transform[t].T @ (
                     solutions[-1] 
-                    - solutions[-1] @ self.control_transform @ torch.linalg.inv(
-                        self.control_transform.T @ solutions[-1] @ self.control_transform
-                        + self.control_penalty
-                    ) @ self.control_transform.T @ solutions[-1]
-                ) @ self.state_transform + self.state_penalty
+                    - solutions[-1] @ self.control_transform[t] @ torch.linalg.inv(
+                        self.control_transform[t].T @ solutions[-1] @ self.control_transform[t]
+                        + self.control_penalty[t]
+                    ) @ self.control_transform[t].T @ solutions[-1]
+                ) @ self.state_transform[t] + self.state_penalty[t]
             )
             solutions.append(solution)
         solutions = dict(zip(range(len(solutions)), solutions[::-1]))
@@ -148,9 +157,9 @@ class LQGMDP(MDP):
         state = self.reset()[:, :-1]
         for t in range(self.nr_steps):
             feedback_gain_matrix = torch.linalg.inv(
-                self.control_transform.T @ riccati_solution[t + 1] @ self.control_transform
-                + self.control_penalty
-            ) @ self.control_transform.T @ riccati_solution[t + 1] @ self.state_transform
+                self.control_transform[t].T @ riccati_solution[t + 1] @ self.control_transform[t]
+                + self.control_penalty[t]
+            ) @ self.control_transform[t].T @ riccati_solution[t + 1] @ self.state_transform[t]
             action = -torch.matmul(state, feedback_gain_matrix.T)
             state, reward = self.step(action)
             state = state[:, :-1]
